abab5c34c921ae5720da18653dac932c4e0b162f
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2024-01-02 17:08:13 +0100

    Update ChorusVoice.h
    Added chorusVoice from test project into main

--- Source/ChorusVoice.h
+++ Source/ChorusVoice.h
@@ -9,3 +9,72 @@
 */
 
 #pragma once
+#include "CircularBuffer.h"
+#include <JuceHeader.h>
+
+class ChorusVoice
+{
+public:
+    ChorusVoice(int nrofChannels, float maxDelayLengthSec, int sampleRate) :
+        m_delayLine(nrofChannels, maxDelayLengthSec, sampleRate),
+        m_lfo([](float x) {return std::sinf(x); }, 128),
+        m_smoothedSample(0.0f),
+        m_smoothedDelay(0.0f),
+        m_currentSample(0.0f),
+        m_currentDelay(0.0f),
+        m_delayAltered(false)
+    {
+        m_lfo.setFrequency(0.15f);
+    }
+    ~ChorusVoice() {}
+
+    //========================================================================
+    void Prepare(const juce::dsp::ProcessSpec& spec, float delayInSamples)
+    {
+        m_delayLine.prepare(spec.sampleRate, delayInSamples);
+        m_lfo.prepare(spec);
+        m_smoothedSample.reset(spec.sampleRate, 0.000002);
+        m_smoothedSample.setCurrentAndTargetValue(0.0f);
+    }
+
+    void Update(int channel, float delayInSeconds, float sampleRate, float depth)
+    {
+        if (!m_smoothedSample.isSmoothing())
+        {
+            float lfoOut{ m_lfo.processSample(1.0f) };
+            float maxDepth{ 0.5f + (depth * 0.5f) };
+            float minDepth{ 0.5f - (depth * 0.5f) };
+            float lfoDepth{ juce::jmap(lfoOut, -1.0f, 1.0f, minDepth + 0.001f, maxDepth) };
+            float delayInSamples = (delayInSeconds * lfoDepth) * sampleRate;
+            m_smoothedSample.setCurrentAndTargetValue(m_delayLine.Read(channel));
+            m_delayLine.SetDelayLength(delayInSamples);
+            m_smoothedSample.setTargetValue(m_delayLine.Read(channel));
+        }
+    }
+
+    void Write(int channel, float bufferSample)
+    {
+        m_delayLine.Write(channel, bufferSample);
+        m_currentSample = m_delayLine.Read(channel); // breaks the process for some reason when removed
+    }
+
+    float Read(int channel)
+    {
+        if (m_smoothedSample.isSmoothing())
+        {
+            return m_smoothedSample.getNextValue();
+        }
+    }
+
+private:
+    CircularBuffer m_delayLine;
+    juce::dsp::Oscillator<float> m_lfo;
+    juce::SmoothedValue<float> m_smoothedSample;
+    juce::SmoothedValue<float> m_smoothedDelay;
+
+    float m_currentSample;
+
+    float m_currentDelay;
+
+    bool m_delayAltered;
+};
