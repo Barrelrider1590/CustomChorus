8aeaef44867d0d0f12cac3d5aa5d624d052e4675
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2024-01-22 09:05:44 +0100

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,11 +11,12 @@
 
 //==============================================================================
 CustomChorusAudioProcessorEditor::CustomChorusAudioProcessorEditor (CustomChorusAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p),
+    m_lookAndFeel(juce::Colour(20, 20, 20))
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (800, 400);
 }
 
 CustomChorusAudioProcessorEditor::~CustomChorusAudioProcessorEditor()
@@ -26,11 +27,11 @@ CustomChorusAudioProcessorEditor::~CustomChorusAudioProcessorEditor()
 void CustomChorusAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (m_lookAndFeel.GetBackgroundColour());
 
-    g.setColour (juce::Colours::white);
+    g.setColour (juce::Colours::goldenrod);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Custom Chorus", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void CustomChorusAudioProcessorEditor::resized()
